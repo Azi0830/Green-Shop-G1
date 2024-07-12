d4348b5c667e1b9dc40d5753b6577d25a65649a8
Azi0830 <turakulov.t.a@gmail.com>
2024-07-12 12:16:16 +0900

    div

--- src/components/home/category/body/card/index.jsx
+++ src/components/home/category/body/card/index.jsx
@@ -1,8 +1,19 @@
+import {
+  SearchOutlined,
+  HeartOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
+
 const Card = ({ title, main_image, price }) => {
   return (
-    <div className="w-[258px]">
+    <div className="w-[258px] ">
       <div className="w-full h-fit bg-[#fbfbfb]">
         <img src={main_image} alt="sss" />
+        <div className="absolute flex gap-2 bg-red-300">
+          <ShoppingCartOutlined />
+          <HeartOutlined />
+          <SearchOutlined />
+        </div>
       </div>
       <h3>{title}</h3>
       <p className="font-bold text-[#46A358]">$ {price}</p>
